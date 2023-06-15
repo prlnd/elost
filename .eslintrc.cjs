03469804b08bd9fe062af8eee2c04eb92aa88f2f
prlnd <palffy.rolland123@gmail.com>
2023-06-15 04:12:27 +0300

    Initialize project

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+/* eslint-env node */
+
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['tsconfig.json', 'tsconfig.node.json'],
+  },
+  plugins: ['react-refresh', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+  },
+  overrides: [
+    {
+      extends: ['plugin:@typescript-eslint/disable-type-checked'],
+      files: ['!src'],
+    },
+  ],
+};
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Elost</h1>;
+}
+
+export default App;
